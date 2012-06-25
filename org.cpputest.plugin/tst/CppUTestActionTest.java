b4a528d3ecb8d8b3be977882d36e0eee0f242268
Terry Yin <terry.yinzhe@gmail.com>
2012-06-25 23:35:15 +0800

    add maven scripts.

---
+++ org.cpputest.plugin/tst/CppUTestActionTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CppUTestActionTest {
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectionChanged() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDispose() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+}
