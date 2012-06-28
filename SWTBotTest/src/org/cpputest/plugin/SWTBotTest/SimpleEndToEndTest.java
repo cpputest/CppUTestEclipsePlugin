a5ad080681d6e34eaf81ffeb1fdf7e70fbf6f7d6
Terry Yin <terry.yinzhe@gmail.com>
2012-06-28 16:57:06 +0800

    First acceptance test!!!!

---
+++ SWTBotTest/src/org/cpputest/plugin/SWTBotTest/SimpleEndToEndTest.java
@@ -0,0 +1,76 @@
+package org.cpputest.plugin.SWTBotTest;
+
+import static org.junit.Assert.*;
+
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEclipseEditor;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEditor;
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.eclipse.ui.PlatformUI;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class SimpleEndToEndTest {
+
+	private SWTWorkbenchBot bot = new SWTWorkbenchBot();
+
+	@Test
+	public void testCppUTestMenuExist() {
+		bot.menu("CppUTest");
+	}
+	@Ignore("still under development")
+	@Test
+	public void testCopyEmptyStubToClipboard() {
+		bot.perspectiveByLabel("C/C++").activate();
+		createCppProject();
+		SWTBotEclipseEditor editor = createNewCppFile("example.h", "void fun(void);\n");
+		editor.selectLine(0);
+		editor.contextMenu("CppUTest").menu("Copy Empty Stub To Clipboard").click();		
+		assertEquals("abcde", getClipboardContent());
+	
+	}
+	private void createCppProject() {
+		bot.menu("File").menu("New").menu("Project...").click();
+		SWTBotShell shell = bot.shell("New Project");
+		shell.activate();
+		bot.tree().expandNode("C/C++").select("C++ Project");
+		bot.button("Next >").click();
+ 
+		bot.textWithLabel("Project name:").setText("GeneralProjectForTesting");
+		bot.button("Finish").click();
+	}
+	private String getClipboardContent() {
+		SWTBotEclipseEditor temp_editor = createNewCppFile("temp_for_clipboard.h", "");
+		temp_editor.contextMenu("Paste").click();
+		String content = temp_editor.getText();
+		temp_editor.close();
+		return content;
+	}
+	private SWTBotEclipseEditor createNewCppFile(String fileName, String content) {
+		bot.menu("File").menu("New").menu("Other...").click();
+		SWTBotShell shell;
+		shell = bot.shell("New");
+		shell.activate();
+		bot.tree().expandNode("C/C++").select("Header File");
+		bot.button("Next >").click();
+		bot.textWithLabel("Header file:").setText(fileName);
+		bot.button("Finish").click();
+		
+		SWTBotEclipseEditor editor = bot.editorByTitle(fileName).toTextEditor();
+		editor.selectRange(0, 0, 1000);
+		editor.typeText(content);
+		editor.save();
+		return editor;
+	}
+
+
+}
