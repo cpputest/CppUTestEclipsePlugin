175ad5539a2e4cbfad436f3dc4a5b1863e168140
Terry Yin <terry.yinzhe@gmail.com>
2012-06-29 17:51:44 +0800

    fix

---
+++ SWTBotTest/src/org/cpputest/plugin/SWTBotTest/SimpleEndToEndTest.java
@@ -0,0 +1,13 @@
+package org.cpputest.plugin.SWTBotTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleEndToEndTest {
+
+	@Test
+	public void test() {
+	}
+
+}
