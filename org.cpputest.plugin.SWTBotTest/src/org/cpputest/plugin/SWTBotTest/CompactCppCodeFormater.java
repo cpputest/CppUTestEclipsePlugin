c78b7306256a7336df8f2ea6da80ced21342122b
Terry Yin <terry.yinzhe@gmail.com>
2012-07-01 08:27:06 +0800

    fix

--- org.cpputest.plugin.SWTBotTest/src/org/cpputest/plugin/SWTBotTest/CompactCppCodeFormater.java
+++ org.cpputest.plugin.SWTBotTest/src/org/cpputest/plugin/SWTBotTest/CompactCppCodeFormater.java
@@ -2,7 +2,6 @@ package org.cpputest.plugin.SWTBotTest;
 
 public class CompactCppCodeFormater implements ICppCodeFormater {
 
-	@Override
 	public String format(String clipboardContent) {
 		return clipboardContent;
 	}
